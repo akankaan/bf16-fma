8462f639795ab016b3662a03c248116fbeb1b70b
akankaan <ka563@cornell.edu>
2026-08-17 22:50:43 -0400

    Changed FMA IO testbench to accept fma_result_valid signal from pipelined core

--- tb/bf16_fma_io_tb.sv
+++ tb/bf16_fma_io_tb.sv
@@ -17,6 +17,7 @@ module bf16_fma_io_tb;
     logic [15:0] in_data;
     logic [15:0] a, b, c;
     logic        operands_valid;
+    logic        fma_result_valid;
     logic [15:0] fma_result;
     logic [7:0]  out_data;
     logic        result_valid;
@@ -26,16 +27,17 @@ module bf16_fma_io_tb;
 
     bf16_fma_io dut
     (
-        .clk            (clk),
-        .rst_n          (rst_n),
-        .in_data        (in_data),
-        .a              (a),
-        .b              (b),
-        .c              (c),
-        .operands_valid (operands_valid),
-        .fma_result     (fma_result),
-        .out_data       (out_data),
-        .result_valid   (result_valid)
+        .clk              (clk),
+        .rst_n            (rst_n),
+        .in_data          (in_data),
+        .a                (a),
+        .b                (b),
+        .c                (c),
+        .operands_valid   (operands_valid),
+        .fma_result       (fma_result),
+        .fma_result_valid (fma_result_valid),
+        .out_data         (out_data),
+        .result_valid     (result_valid)
     );
 
     always #5 clk = ~clk;
@@ -49,14 +51,14 @@ module bf16_fma_io_tb;
 
     initial begin
         // Reset before test
-        rst_n = 0; in_data = '0; fma_result = '0;
+        rst_n = 0; in_data = '0; fma_result = '0; fma_result_valid = '0;
         @(posedge clk);                                   
 
         // Stands in for core output, validation with real core
         // is done in top file's testbench.
         fma_result = 16'hABCD;
 
-        // Load first operan triple
+        // Load first operand triple
         @(negedge clk); rst_n = 1; in_data = 16'h1111;
         @(posedge clk);
         @(negedge clk); in_data = 16'h2222;
@@ -69,13 +71,13 @@ module bf16_fma_io_tb;
         check((c == 16'h3333), "c loaded");
 
         // Start sending second pair for first output and second input overlap
-        @(negedge clk); in_data = 16'h4444;
+        @(negedge clk); in_data = 16'h4444; fma_result_valid = 1'b1;
         @(posedge clk); #1;
     
         check((out_data == 8'hCD), "first out byte correct");
         check((result_valid == 1), "output result is valid");
 
-        @(negedge clk); in_data = 16'h5555;
+        @(negedge clk); in_data = 16'h5555; fma_result_valid = 1'b0;
         @(posedge clk); #1;
 
         check((out_data == 8'hAB), "second out byte correct");
@@ -89,6 +91,9 @@ module bf16_fma_io_tb;
 
         fma_result = 16'h4567;
 
+        @(negedge clk);
+        fma_result_valid = 1'b1;
+
         check((a == 16'h4444), "a loaded");
         check((b == 16'h5555), "b loaded");
         check((c == 16'h6666), "c loaded");
@@ -98,6 +103,9 @@ module bf16_fma_io_tb;
         check((out_data == 8'h67), "first out byte correct");
         check((result_valid == 1), "output result is valid");
 
+        @(negedge clk);
+        fma_result_valid = 1'b0;
+        
         @(posedge clk); #1;
 
         check((out_data == 8'h45), "second out byte correct");
